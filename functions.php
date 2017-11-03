c36a94ead21307994c62a7dc8d6b77de6923939e
Mikko Siikaniemi <mikko@mikrogramma.fi>
2017-11-03 16:27:16 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@
 add_action( 'wp_enqueue_scripts', 'vaasawebdev_styles_and_scripts', 20 );
 
 function vaasawebdev_styles_and_scripts() {
-  wp_enqueue_style( 'vaasawebdev-styles', get_stylesheet_uri() );
-  wp_enqueue_script( 'vaasawebdev-scripts', get_template_directory_uri() . '/js/theme.min.js', array( 'jquery' ), false, true );
+	wp_enqueue_style( 'vaasawebdev-styles', get_stylesheet_uri() );
+	wp_enqueue_script( 'vaasawebdev-scripts', get_template_directory_uri() . '/js/theme.min.js', array( 'jquery' ), false, true );
 }
 
 // ---------------------------------------------------------------------------
@@ -19,23 +19,23 @@ add_action( 'init', 'mikrogramma_disable_emojis' );
 
 function mikrogramma_disable_emojis() {
 
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	add_filter( 'emoji_svg_url', '__return_false' );
-  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
 }
 
 function disable_emojicons_tinymce( $plugins ) {
-  if ( is_array( $plugins ) ) {
-    return array_diff( $plugins, array( 'wpemoji' ) );
-  } else {
-    return array();
-  }
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
 }
 
 // ---------------------------------------------------------------------------
@@ -68,18 +68,18 @@ function mikrogramma_move_jquery_to_footer() {
 		return;
 	}
 
-  wp_scripts()->add_data( 'jquery', 'group', 1 );
-  wp_scripts()->add_data( 'jquery-core', 'group', 1 );
-  wp_scripts()->add_data( 'jquery-migrate', 'group', 1 );
+	wp_scripts()->add_data( 'jquery', 'group', 1 );
+	wp_scripts()->add_data( 'jquery-core', 'group', 1 );
+	wp_scripts()->add_data( 'jquery-migrate', 'group', 1 );
 }
 
 add_filter( 'wp_default_scripts', 'mikrogramma_remove_jquery_migrate' );
 
 function mikrogramma_remove_jquery_migrate( &$scripts) {
-  if( ! is_admin() ) {
-    $scripts->remove( 'jquery' );
-    $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4' );
-  }
+	if( ! is_admin() ) {
+		$scripts->remove( 'jquery' );
+		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4' );
+	}
 }
 
 // ---------------------------------------------------------------------------
@@ -89,14 +89,14 @@ function mikrogramma_remove_jquery_migrate( &$scripts) {
 add_action( 'widgets_init', 'vaasawebdev_widgets_init' );
 
 function vaasawebdev_widgets_init() {
-  register_sidebar( array(
-    'name' 					=> 'Sidebar',
-    'id' 						=> 'widgets',
-    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h4 class="widgettitle">',
-		'after_title'   => '</h4>',
-    ) );
+	register_sidebar( array(
+		'name'           => 'Sidebar',
+		'id'             => 'widgets',
+		'before_widget'  => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'   => '</aside>',
+		'before_title'   => '<h4 class="widgettitle">',
+		'after_title'    => '</h4>',
+	) );
 }
 
 // ---------------------------------------------------------------------------
